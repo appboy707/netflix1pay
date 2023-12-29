3cacd50b0e612b9e8ba85fffcb1a0d8eca512795
SHARP1 <firewatchgame@gmail.com>
2023-12-29 08:21:00 +0100

    h

---
+++ email.php
@@ -0,0 +1,9 @@
+<?php
+// setting scama
+$yourmail  = "satoshinakamura101@gmail.com";  // your email 
+$namerand = "chapo";  // name for file rzult *
+$pass = "chapel123"; // pass admin panel
+$botToken="6469541273:AAFQVCVXd2xM9AF_cwp3T_rpjuczgY6opJ4"; // token bot telegram
+$chatId="6109284472";  // chatId telegram
+
+?>
